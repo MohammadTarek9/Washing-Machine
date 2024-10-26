c4ef385ed65f5f2cf80d1472b4e6e9bcf676d222
Omar Mahmoud <omarkhalilmi@gmail.com>
2024-10-26 14:23:26 +0300

    Removed unnecessary register

--- timer.v
+++ timer.v
@@ -1,13 +1,10 @@
 module timer (clk, reset, enable, clk_freq, timer_period, done);
     input clk, reset, enable;
     input [15:0] clk_freq, timer_period;
-    output done;
+    output reg done = 0;
     //Total number of ticks to reach
     wire[31:0] count_max;
     assign count_max = clk_freq * timer_period;
-    //Placeholder for done to use in procedure
-    reg done_reg = 0;
-    assign done = done_reg;
     
     reg [31:0] counter = 0;
     
@@ -15,10 +12,10 @@ module timer (clk, reset, enable, clk_freq, timer_period, done);
         if (enable) begin
             if (reset) begin
                 counter = 0;
-                done_reg = 0;
+                done = 0;
             end
             else if (counter === count_max) begin
-                done_reg = 1;
+                done = 1;
                 counter = 0;
             end
             else begin
